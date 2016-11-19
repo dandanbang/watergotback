29faa17704244d7423048ed16b61bdfc06e7435b
Daniel Chen <t_chend@calvisitor-10-105-164-106.calvisitor.1918.berkeley.edu>
2016-11-19 11:11:31 -0800

    adding animation

--- js/drought_animate.js
+++ js/drought_animate.js
@@ -1,15 +1,14 @@
 $( document ).ready(function() {
   var images = Array("js/cali_drought/20111220_ca_none.png", "js/cali_drought/20120306_ca_none.png", "js/cali_drought/20130219_ca_none.png", "js/cali_drought/20140527_ca_none.png", "js/cali_drought/20150120_ca_none.png")
   var display_dates = Array("2011", "2012", "2013", "2014", "2015", "2015")
-  var imgc =0;
+  var imgc = 1000;
   setImage()
 
   function setImage() {
       var newimage = images[imgc];
-      if (imgc < 5)  {
-        imgc++;
+      for (i = 0; i < imgc; i++)  {
         $('#drought').attr("src", newimage).fadeIn(50);
-        $("#year").html('<h2>' + display_dates[imgc]+'<h2>');
+        $("#year").html('<h4>' + display_dates[imgc]+'<h4>');
       setTimeout(setImage, 1000);
       }
   };
