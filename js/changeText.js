819e99bd02e4e989f308af369f72d54043c00643
Daniel Chen <t_chend@calvisitor-10-105-164-106.calvisitor.1918.berkeley.edu>
2016-11-19 15:21:16 -0800

    animating the 5 missing metrics

---
+++ js/changeText.js
@@ -0,0 +1,35 @@
+$('.data-well').hover(
+    function() {
+        $('#without').text("Water managers can't support the state’s economy.");
+        $('#with').text("Sustainably management of groundwater for their direct use to support the state’s economy.");
+    }
+);
+
+
+$('.data-groundwater').hover(
+    function() {
+        $('#without').text("Unable to predict early warning of water.");
+        $('#with').text("Allows proactive management for actions.");
+    }
+);
+
+$('.data-meter').hover(
+    function() {
+        $('#without').text("Failures in providing groundwater conditions.");
+        $('#with').text("Ensures a sustainable ground supply.");
+    }
+);
+
+$('.data-quality').hover(
+    function() {
+        $('#without').text("Unable to identify contaimination sources.");
+        $('#with').text("Addresses or alleviate potential contamination problems before it becomes too costly to mitigate or leads to loss of groundwater supply.");
+    }
+);
+
+$('.data-model').hover(
+    function() {
+        $('#without').text("Incohesion in water supply and lack of knowledge about the infrastructure");
+        $('#with').text("Maintain healthier aquifers that can continue to supply water.");
+    }
+);
