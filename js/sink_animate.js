9693cd7259e5022845304444a98966d669f6f93f
leftan <leftan12121@gmail.com>
2016-11-19 16:08:57 -0800

    awhtta

--- js/sink_animate.js
+++ js/sink_animate.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   var images = Array("js/cali_sink/2002_cropped.svg", "js/cali_sink/2003_cropped.svg", "js/cali_sink/2004_cropped.svg", "js/cali_sink/2005_cropped.svg", "js/cali_sink/2006_cropped.svg",
                      "js/cali_sink/2007_cropped.svg", "js/cali_sink/2008_cropped.svg", "js/cali_sink/2009_cropped.svg", "js/cali_sink/2010_cropped.svg", "js/cali_sink/2011_cropped.svg",
                      "js/cali_sink/2012_cropped.svg", "js/cali_sink/2013_cropped.svg", "js/cali_sink/2014_cropped.svg", "js/cali_sink/2015_cropped.svg", "js/cali_sink/2016_cropped.svg")
-  var display_dates = Array("2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016")
+  var display_dates = Array("2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2016")
   var imgc =0;
   setImage()
 
